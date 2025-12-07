8557a655a2eb46d77902339af8780e4967227b71
zyhwitness <zyhwitness23@gmail.com>
2025-12-07 23:42:43 +0800

    feat: api-key、chat接口

--- springai-alibaba-quick-start/src/main/java/com/sangeng/controller/ZhiPuChatController.java
+++ springai-alibaba-quick-start/src/main/java/com/sangeng/controller/ZhiPuChatController.java
@@ -1,15 +1,26 @@
 package com.sangeng.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/zhipu")
 public class ZhiPuChatController {
 
+    private final ChatModel chatModel;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello zhipu";
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(name = "prompt") String prompt) {
+        return chatModel.call(prompt);
     }
 }
