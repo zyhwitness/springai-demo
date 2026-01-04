5ddb91e12eb94512a2caef74f88f4d0bffd4a075
zyhwitness <zyhwitness23@gmail.com>
2026-01-04 17:13:54 +0800

    feat: 统一设置官方Advisor-MessageChatMemoryAdvisor       实现对话记录存储

---
+++ springai-alibaba-quick-start/src/main/java/com/sangeng/controller/ZhiPuChatMemoryController.java
@@ -0,0 +1,44 @@
+package com.sangeng.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chatmemory")
+public class ZhiPuChatMemoryController {
+
+    private final ChatClient chatClient;
+
+    // 构造器注入
+    public ZhiPuChatMemoryController(ChatClient.Builder builder) {
+        // 创建 MessageWindowChatMemory
+        MessageWindowChatMemory messageWindowChatMemory = MessageWindowChatMemory
+                .builder()
+                .build();
+
+        //创建 MessageChatMemoryAdvisor
+        MessageChatMemoryAdvisor messageChatMemoryAdvisor = MessageChatMemoryAdvisor
+                .builder(messageWindowChatMemory)
+                .build();
+
+        this.chatClient = builder
+                .defaultAdvisors(messageChatMemoryAdvisor)
+                .build();
+    }
+
+    @GetMapping("/messageChatMemory")
+    public String messageChatMemory(@RequestParam(name = "query") String query, @RequestParam(name = "sessionId") String sessionId) {
+        return chatClient.prompt()
+                .user(query)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, sessionId))
+                .call().content();
+    }
+
+}
